326e48ce3b2717097ab9803ca746e46631c796d4
Anthony J Burton <ant@coshx.com>
2012-06-01 19:02:58 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+IMLS Project Beta Site 
+
+A Rails site utilizing Blacklight and Solr for the purpose of connecting
+presidential collections. 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imls::Application.config.secret_token = 'f6e3b05933ee9442eb6920c2cc82b144d7cab747b619cfa4c1050546d40efe1dfdd6da4ff2971812fb6fd1343daf8b55c98b0a1863d6b8a4c6331827f73b7bd8'
