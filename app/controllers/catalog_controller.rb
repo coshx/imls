c7b02b4550253c3685edc8f2fec445503c23c51d
Ryan Ahearn <ryan@coshx.com>
2012-09-12 12:59:40 -0400

    Copies appropriate fields to _sort fields for blacklight sorting

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -19,6 +19,7 @@ class CatalogController < ApplicationController
     config.index.record_display_type = 'format'
     config.add_index_field 'creator', :label => 'Creator:'
     config.add_index_field 'subject', :label => 'Subject:'
+    config.add_index_field 'date', :label => 'Date:'
 
 
     # solr field configuration for document/show views
